e8272134148ebc70b592879b902687f9dc6e6c6d
fring-12 <banedictfring12@gmail.com>
2023-09-23 11:45:59 +0600

    layout added

---
+++ src/layouts/index.tsx
@@ -0,0 +1,24 @@
+import { FC, useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import ScrollButton from "./ScrollButton";
+import { Outlet, useLocation } from "react-router-dom";
+import animation from "../helpers/animation";
+
+const Layout: FC = () => {
+  let { pathname } = useLocation();
+  useEffect(() => animation(), [pathname]);
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+        <ScrollButton />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
